169f87fa121caca4bf44a69cf722490886049ab0
Nakai <nakai.hamlin18@gmail.com>
2021-04-15 04:38:39 -0400

    commit tree e68f95ae8d4d81c6074bbfcdc50b6fd76b7de179

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+require_relative '../models/concerns/slugable.rb'
+
+class Song < ActiveRecord::Base 
+    belongs_to :artist
+    has_many :song_genres
+    has_many :genres, through: :song_genres
+    extend Slugable::ClassMethods
+    include Slugable::InstanceMethods
+end
